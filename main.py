5f0fbebc932d16dc44f4bc54ab9521f68b6075de
Foster_Toster <gribnoysalatik@gmail.com>
2021-04-09 11:36:38 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+from gihub_webhook import git
+
+app.register_blueprint(git, url_prefix="/github_webhook")
+
+if __name__ == '__main__':
+    serve(app, listen="*:5550")
+    # app.run(host='0.0.0.0', port=5550)
